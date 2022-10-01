0adf28cc601f4cc9f85d8780f9f1e0ce1d373293
Atzcck <alptazecicek@gmail.com>
2022-10-02 00:51:20 +0300

    Comments added.

--- app/src/main/java/com/alptazecicek/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/alptazecicek/coroutineexample/MainActivity.kt
@@ -9,7 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // counter initilized.
         var counter = 0
+
+        // Coroutine Scope added in to infinite while loop.
         while(true) {
             CoroutineScope(Dispatchers.IO).launch {
                 val answer = doNetworkCall()
@@ -19,8 +23,10 @@ class MainActivity : AppCompatActivity() {
                 counter += 1
             }
         }
+        //this code is unreachable because of main thread infinite loop.
         println(counter)
     }
+    //suspend function defined with 2 sec delay.
     suspend fun doNetworkCall(): String {
         delay(2000L)
         return "NetworkCallAnswer"
